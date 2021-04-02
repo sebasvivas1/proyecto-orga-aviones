70004050a4bb308533acf1ef46cef1d6fa61f04b
Yasmin Hammoud <yasmin.hammoud@correo.unimet.edu.ve>
2021-04-02 19:22:06 -0400

    falta comprobar su funcionamiento, recordar imprimir la lista de diccionarios para verificar si se ordenaron correctamente

--- functions.py
+++ functions.py
@@ -25,16 +25,34 @@ def binary_search_name_model(array, element, str):
                 first = mid+1
                 
     return result
+
+def check_if_exists(array, element, str):
+    """[Verifica si el elemento introducido se encuentra en la lista dada]
+
+    Args:
+        product_list ([list]): [Lista]
+        product ([str]): [Nombre del elemento a buscar]
+
+    Returns:
+        [None o impresión]
+    """
+    first = 0 
+    last = len(array)-1
+    index = -1 
+    result = False
+
+    while (first <= last):
+        mid = (first+last)//2 
+        if (array[mid][str]) == element:
+            result =  True 
+            return result
+        else:
+            if element < array[mid][str]:
+                last = mid -1
+            else: 
+                first = mid+1            
+    return result
     
-
-
-
-def checkIfFull(array):
-    arrayFull = False 
-    if array[0]!= None and array[1]!= None:
-        arrayFull = True
-    return arrayFull
-
 def file_existence(file_name):
     """[Verifica si el archivo existe o no]
 
@@ -46,15 +64,6 @@ def file_existence(file_name):
             return True
     except FileNotFoundError as e:
         return False 
-"""
-def serial():
-    while True:
-        try:
-            serial= input("Ingrese el Serial del Avión: ")
-        except:
-            pass
-    return serial 
-"""
 
 def model_name():
     while True:
@@ -89,15 +98,15 @@ def plane_name():
 def validacion_serial():
     loop = True
     while loop:
-        serial = input("Por favor ingrese un serial: ")
+        serial = input("Ingrese el Serial del Avión: ")
         primera_letra = serial[0]
-        numeros = serial[1:7]
-        if len(serial)>8:
-            print("Serial invalido (mayor de 8 caracteres) ")
+        numeros = serial[1:8]
+        if len(serial) != 9:
+            print("Serial inválido (Debe ingresar 9 carácteres) ")
         elif primera_letra.isupper()==False:
-            print("Serial invalido (primera letra no es capital)")
+            print("Serial inválido (Primera letra no está en mayúscula)")
         elif numeros.isdigit()==False:
-            print("Serial invalido (no agrego numeros)")
+            print("Serial inválido (No agregó numeros)")
         else:
             loop = False
     return serial
@@ -106,12 +115,9 @@ def sortNameList(arr):
     orderedList = sorted(arr, key = lambda k: k['name'])
     return orderedList
 
-
 def sortModelList(arr):
     orderedList = sorted(arr, key = lambda k: k['model'])
     return orderedList
-    # for x in orderedList:
-    #     print((x['model']))
     
 def insertToList(key1, val, key2, serial):
     dict = {}
@@ -119,26 +125,24 @@ def insertToList(key1, val, key2, serial):
     dict[key2] = serial
     return dict.copy()
     
-
 def numeros():
     loop = True
     num_operacion = 0
     while loop:
         try:
             num_operacion = int(input('''
-        Por favor ingrese los siguientes numeros si desea:
+Por favor, ingrese el número correspondiente a la operación que desea ejecutar:
 
-        1. Ingresar un nuevo avion
-        2. Buscar un avion
-        3. Asignarle un piloto a un avion
-        4. Liberar un avion
-        5. Eliminar un avion
-
-        '''))
+  1. Ingresar un nuevo avion
+  2. Buscar un avion
+  3. Asignarle un piloto a un avion
+  4. Liberar un avion
+  5. Eliminar un avion
+  >>> '''))
         except ValueError:
-            print("\nIngrese un numero por favor")
+            print("\nIngrese un número, por favor")
 
-        if num_operacion==1 or num_operacion==2 or num_operacion==3 or num_operacion==4 or num_operacion==5 or num_operacion ==7:
+        if num_operacion==1 or num_operacion==2 or num_operacion==3 or num_operacion==4 or num_operacion==5 :
             loop = False
             return num_operacion
         else:
@@ -148,11 +152,15 @@ def salida():
     loop = True
     while True:
         try:
-            chao= int(input("Ingrese 0 si desea salir o 1 si desea volver a empezar\n ==>"))
+            chao= int(input("""
+Ingrese: 
+  1. Para volver a empezar
+  2. Para salir
+  >>> """))
         except ValueError:
-            print("\nPor favor ingrese un numero por favor")
-        if chao==1 or chao==0:
+            print("\nPor favor, ingrese un número")
+        if chao==1 or chao==2:
             loop = False
             return chao
         else:
-            print("\Por favor ingrese un numero valido")
+            print("\Por favor, ingrese un número válido")
